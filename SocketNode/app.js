a800a97c14ac2bca1c16f50f65135e175ba295c0
李家太 <2862260725@qq.com>
2017-06-28 15:35:01 +0800

    none

--- SocketNode/app.js
+++ SocketNode/app.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var app = express();
+var http = require('http');//依赖http
+http.createServer(function(req, res) {//创建一个服务器 
+    res.writeHead(200, {"Content-Type": "text/html"}); // not necessary but better set
+    res.end('<div>Hello world!</div>');
+}).listen(3000);
 
-app.get('/hello', function (req, res) {
-  res.setHeader( 'Access-Control-Allow-Origin','*');
-  res.json({
-  	name: 'lijiatai'
-  });
-  res.end('')
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({
+    port: 3000
 });
-
-app.listen(3000);
-console.log('Example app l, port');
+wss.on('connection', function(ws) {
+    console.log('someone just coming, now has %d client', wss.clients.length
+);
+    ws.on('message', function(data) {
+        wss.clients.forEach(function(client) {
+            client.send(data);
+        });
+    });
+    ws.on('close', function() {
+        console.log('someone just leave, now has %d client', wss.clients.length);
+    });
+    ws.on('error', function(err) {
+        console.log(err);
+    });
+});
+console.log('ws estriblish success');
